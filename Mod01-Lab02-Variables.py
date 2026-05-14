12b10da347e4f8d9a625ab87a6009f8fa0436799
RRoot <rootr@uw.edu>
2026-05-13 19:32:24 -0700

    Add files via upload

---
+++ Mod01-Lab02-Variables.py
@@ -0,0 +1,15 @@
+# ------------------------------------------------------------------------------------------ #
+# Title: Mod01-Lab01-Variables
+# Desc: This lab demonstrates how to create a use variables
+# Change Log: (Who, When, What)
+#   RRoot,1/1/2030,Created Script
+#   <Your Name Here>,<Date>, <Activity>
+# ------------------------------------------------------------------------------------------ #
+"""
+* Create variables with descriptive names
+* Use the print() function to display the student's full name
+"""
+
+student_first_name_str = "Vic"
+student_last_name_str = "Vu"
+print(student_first_name_str, student_last_name_str)
